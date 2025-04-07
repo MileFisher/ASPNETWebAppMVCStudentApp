9040e4cfed461117bba08b501790cc35ede016ca
MileFisher <broken101284@gmail.com>
2025-04-07 23:39:06 +0700

    all done to exercise 4(but in printing report should handle some error)

---
+++ ASPNETWebAppMVCStudentApp/Controllers/ReportController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Reporting.WebForms;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using ASPNETWebAppMVCStudentApp.Models;
+
+namespace ASPNETWebAppMVCStudentApp.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly SchoolDBEntities _context;
+
+        public ReportController()
+        {
+            _context = new SchoolDBEntities(); // Your EF context
+        }
+
+        public ActionResult Index()
+        {
+            var model = new CourseViewModel
+            {
+                Courses = _context.Courses.Select(c => new SelectListItem
+                {
+                    Value = c.CourseID.ToString(),
+                    Text = c.Title
+                })
+            };
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Generate(int SelectedDepartmentID, string action)
+        {
+            var localReport = new LocalReport();
+            localReport.ReportPath = Server.MapPath("~/Reports/CourseReport.rdlc");
+
+            var courses = _context.Courses
+                .Where(c => c.DepartmentID == SelectedDepartmentID)
+                .Select(c => new
+                {
+                    c.CourseID,
+                    c.Title,
+                    c.Credits,
+                    c.DepartmentID
+                }).ToList();
+
+            var reportDataSource = new ReportDataSource("CourseDataSet", courses);
+            localReport.DataSources.Add(reportDataSource);
+
+            string reportType = "PDF";
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+            string[] streams;
+            Warning[] warnings;
+
+            var renderedBytes = localReport.Render(
+                reportType,
+                null,
+                out mimeType,
+                out encoding,
+                out fileNameExtension,
+                out streams,
+                out warnings);
+
+            if (action == "export")
+            {
+                return File(renderedBytes, "application/pdf", "CourseReport.pdf");
+            }
+            else // Preview
+            {
+                return File(renderedBytes, "application/pdf");
+            }
+        }
+    }
+}
---
+++ ASPNETWebAppMVCStudentApp/Models/CourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETWebAppMVCStudentApp.Models
+{
+    public class CourseViewModel
+    {
+        public int SelectedCourseID { get; set; }
+        public IEnumerable<SelectListItem> Courses { get; set; }
+    }
+    public class DepartmentViewModel
+    {
+        public int SelectedDepartmentID { get; set; }
+        public IEnumerable<SelectListItem> Departments { get; set; }
+    }
+}
